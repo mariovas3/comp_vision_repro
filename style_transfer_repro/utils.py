9cc55df45d929005eac599294bed245bbaf0d52c
Mario Vasilev <mariovasilev212@gmail.com>
2022-04-12 21:56:56 +0100

    [feat]: add IO functionality for images together with some transform functions;

---
+++ style_transfer_repro/utils.py
@@ -0,0 +1,57 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision.transforms as T
+import torchvision.models as models
+from torchvision.io import read_image
+from PIL.Image import Image
+import os
+np.random.seed(42)
+torch.manual_seed(42)
+
+
+# standardise RGB images using the channel-wise quantites below;
+MAGIC_NUMBERS = {"mean": torch.tensor([0.485, 0.456, 0.406]),
+        "std": torch.tensor([0.229, 0.224, 0.225])}  # as per https://github.com/pytorch/vision/issues/1439
+# to my understanding the MAGIC_NUMBERS are calculated on a subset of ImageNet;
+# the exact subset seems to be lost;
+# Nevertheless the docs (https://pytorch.org/vision/0.11/models.html) suggest that
+# the pretrained models expect the input images to be standardised using the above quantities;
+
+
+CUR_DIR = os.path.dirname(__file__)
+CACHE_DIR = os.path.join(CUR_DIR, "data/")
+# set cache directory for saving pretrained model;
+torch.hub.set_dir(CACHE_DIR)
+
+
+def load_model(name: str, pretrained: bool=False) -> torch.Tensor:
+    assert torch.hub.get_dir() == CACHE_DIR
+    return exec(f"models.{name}(pretrained={pretrained})")
+
+
+def load_image(path: str, transform: nn.Module=None, apply_transform: bool=False) -> torch.Tensor:
+    img = read_image(path)  # gives torch.uint8 type tensor;
+    img_dims = img.shape  # dimensions for tensor are in (C, H, W) format;
+    if apply_transform:
+        if transform is None:
+            transform = nn.Sequential(
+                        T.Resize((720, 1080)),  # change height to 720p and width to 1080p;
+                        T.ConvertImageDtype(torch.float32),  # pretrained models take tensor images with values in [0, 1];
+                        T.Normalize(**MAGIC_NUMBERS)  # then standardise to be consistent with ImageNet input;
+                    )
+        return transform(img)
+    return img
+
+
+def tensor_to_pil(img: torch.Tensor, back_transform: nn.Module=None, apply_transform: bool=False) -> Image:
+    img = img.detach().clone()  # don't touch the actual image as it may be still in training loop;
+    if apply_transform:
+        if back_transform is None:
+            back_transform = nn.Sequential(
+                        T.Normalize(- MAGIC_NUMBERS["mean"] / MAGIC_NUMBERS["std"], 1 / MAGIC_NUMBERS["std"]),
+                        T.ConvertImageDtype(torch.uint8)
+                    )
+        return T.ToPILImage()(back_transform(img))
+    return T.ToPILImage()(img)
+
