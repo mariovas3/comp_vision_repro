fix target bbox computation
now coords are the result of resizing and center cropping, which is done to the images; because of cropping after resizing, we may lose some boxes on the edge if we crop too little, if all boxes in an image lose more than 40% of either their width or height from resizing phase to cropping phase, then we ignore that image; in the train set we ignore 58, in val - 40; in test - 129; so it's not too bad

yolo/data/lit_data.py
@@ -3,7 +3,6 @@ from itertools import chain
 from pathlib import Path
 from typing import List
 
-import smart_open
 import torch
 import torchvision.transforms as T
 from lightning import LightningDataModule
@@ -31,8 +30,11 @@ class LitVOCData(LightningDataModule):
         super().__init__()
         self.grid_dim = grid_dim
         assert grid_dim <= 13, f"{grid_dim=}, but should be <= 13"
-        self.standard_img_dim = grid_dim * 32
-        self.img_transform = get_img_transform(grid_dim=grid_dim)
+        self.crop_size = grid_dim * 32
+        self.resize_size = self.crop_size + 8
+        self.img_transform = get_img_transform(
+            resize_size=self.resize_size, crop_size=self.crop_size
+        )
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.pin_memory = pin_memory
@@ -49,28 +51,33 @@ class LitVOCData(LightningDataModule):
         if not (metadata.DATA_DIR / "train_gt.pkl").exists():
             save_imgs_and_labels(
                 grid_dim=self.grid_dim,
+                resize_size=self.resize_size,
                 split="train",
                 years=self.years,
                 ignore_multibox=self.ignore_multibox,
-                standard_img_dim=self.standard_img_dim,
+                crop_size=self.crop_size,
             )
             save_imgs_and_labels(
                 grid_dim=self.grid_dim,
+                resize_size=self.resize_size,
                 split="val",
                 years=self.years,
                 ignore_multibox=False,
-                standard_img_dim=self.standard_img_dim,
+                crop_size=self.crop_size,
             )
             save_imgs_and_labels(
                 grid_dim=self.grid_dim,
+                resize_size=self.resize_size,
                 split="test",
                 years=("2007",),
                 ignore_multibox=False,
-                standard_img_dim=self.standard_img_dim,
+                crop_size=self.crop_size,
             )
 
         save_anchor_box_dims(
-            years=self.years, standard_img_dim=self.standard_img_dim
+            resize_size=self.resize_size,
+            years=self.years,
+            crop_size=self.crop_size,
         )
         print("DATA PREP DONE!")
 
@@ -130,12 +137,11 @@ class LitVOCData(LightningDataModule):
         )
 
 
-def get_img_transform(grid_dim):
-    img_size = grid_dim * 32
+def get_img_transform(resize_size, crop_size):
     return T.Compose(
         [
-            T.Resize(img_size + 32),
-            T.CenterCrop(img_size),
+            T.Resize(resize_size),
+            T.CenterCrop(crop_size),
             T.ToTensor(),
             T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
@@ -148,26 +154,20 @@ def save_imgs(imgs: List[Image.Image], dir_path: Path):
         im.save(dir_path / f"{idx}.jpg")
 
 
-def read_pil(filename):
-    with smart_open.open(filename, "rb") as f:
-        with Image.open(f) as image:
-            image = image.convert(image.mode)
-            return image
-
-
 def load_imgs(dir_path: Path):
     img_names = sorted(
         dir_path.glob("*.jpg"), key=lambda filename: int(Path(filename).stem)
     )
-    return [read_pil(filename) for filename in img_names]
+    return [utils.read_pil(filename) for filename in img_names]
 
 
 def save_imgs_and_labels(
     grid_dim,
+    resize_size,
     split="train",
     years=("2007",),
     ignore_multibox=True,
-    standard_img_dim=224,
+    crop_size=224,
 ):
     imgs, labels = [], []
     for year in years:
@@ -179,8 +179,9 @@ def save_imgs_and_labels(
             grid_dim=grid_dim,
             num_bbox_elements=5,
             label_to_idx=metadata.LABEL_TO_IDX,
+            resize_size=resize_size,
             ignore_multibox=ignore_multibox,
-            standard_img_dim=standard_img_dim,
+            crop_size=crop_size,
         )
         imgs.extend(ims)
         labels.extend(labs)
@@ -191,7 +192,7 @@ def save_imgs_and_labels(
     utils.save_to_pickle(labels, metadata.DATA_DIR / f"{split}_gt.pkl")
 
 
-def save_anchor_box_dims(years=("2007",), standard_img_dim=224):
+def save_anchor_box_dims(resize_size, years=("2007",), crop_size=224):
     train_sets = []
     for year in years:
         train_data = VOCDetection(
@@ -203,9 +204,11 @@ def save_anchor_box_dims(years=("2007",), standard_img_dim=224):
     all_boxes = torch.tensor(
         utils.get_all_boxes(
             chain(*train_sets),
+            resize_size=resize_size,
             scale_box_dims=True,
-            standard_img_dim=standard_img_dim,
-        )
+            crop_size=crop_size,
+        ),
+        dtype=torch.float32,
     )
 
     print(f"RUNNING KMEANS FOR ANCHOR BOX PRIORS...")

yolo/data/utils.py
@@ -2,9 +2,12 @@ import json
 import pickle
 import random
 from pathlib import Path
-from typing import Literal
+from typing import Literal, MutableSequence, Tuple
 
+import smart_open
 import torch
+from numpy import clip
+from PIL import Image
 
 from yolo.metadata import metadata
 from yolo.model import eval_utils
@@ -16,12 +19,12 @@ def get_labels_and_boxes_and_size(voc_annotation: dict):
     for item in voc_annotation["object"]:
         labels.append(item["name"])
         boxes.append(
-            (
+            [
                 float(item["bndbox"]["xmin"]),
                 float(item["bndbox"]["ymin"]),
                 float(item["bndbox"]["xmax"]),
                 float(item["bndbox"]["ymax"]),
-            )
+            ]
         )
     return labels, boxes, img_size
 
@@ -111,23 +114,26 @@ class Kmeans:
             self.num_runs -= 1
 
 
-def get_all_boxes(dataset, scale_box_dims=False, standard_img_dim=224):
+def get_all_boxes(dataset, resize_size, scale_box_dims=False, crop_size=224):
     all_boxes = []
     for _, info in dataset:
         _, boxes, img_size = get_labels_and_boxes_and_size(info["annotation"])
         if scale_box_dims:
+            new_boxes = []
             for i, box in enumerate(boxes):
-                box = [
-                    c
-                    * standard_img_dim
-                    / (
-                        img_size["width"]
-                        if c_i % 2 == 0
-                        else img_size["height"]
-                    )
-                    for c_i, c in enumerate(box)
-                ]
-                boxes[i] = box
+                # get XYXY after T.Resize and T.CenterCrop applied
+                # to image.
+                box, bad_crop = resize_and_crop_bbox(
+                    bbox=box,
+                    old_w=img_size["width"],
+                    old_h=img_size["height"],
+                    resize_size=resize_size,
+                    crop_size=crop_size,
+                )
+                if bad_crop:
+                    continue
+                new_boxes.append(box)
+            boxes = new_boxes
         all_boxes.extend(boxes)
     return all_boxes
 
@@ -158,6 +164,13 @@ def load_pickle(filepath: Path):
     return obj
 
 
+def read_pil(filename):
+    with smart_open.open(filename, "rb") as f:
+        with Image.open(f) as image:
+            image = image.convert(image.mode)
+            return image
+
+
 def get_unique_labels(dataset) -> set[str]:
     labels = set()
     for data in dataset:
@@ -167,45 +180,25 @@ def get_unique_labels(dataset) -> set[str]:
     return labels
 
 
-def corners_to_midpoint(*coords):
-    xmin, ymin, xmax, ymax = coords
-    centerx = (xmax + xmin) / 2
-    centery = (ymax + ymin) / 2
-    boxwidth = xmax - xmin
-    boxheight = ymax - ymin
-    return centerx, centery, boxwidth, boxheight
-
-
-def midpoint_relative_to_grid(
-    *coords, img_size, grid_dim, standard_img_dim=224
-) -> tuple[tuple]:
-    """returns Tuple of tuples - coords and grid cell (i, j) coords."""
-    x, y, w, h = coords
-    # x and y are in (0, grid_dim)
-    x = grid_dim * x / img_size["width"]
-    y = grid_dim * y / img_size["height"]
-    # i,j represents the cell row and cell column
-    i, j = int(y), int(x)
-    # make x and y in pixel coords in standard image;
-    x, y = x * standard_img_dim / grid_dim, y * standard_img_dim / grid_dim
-    # w and h are in (0, standard_img_dim)
-    # with standard_img_dim=224 for resnet50;
-    w = w / img_size["width"] * standard_img_dim
-    h = h / img_size["height"] * standard_img_dim
-    return (x, y, w, h), (i, j)
-
-
 def get_targets(
     voc_annotation: dict,
     grid_dim,
     num_bbox_elements,
     label_to_idx: dict,
-    standard_img_dim=224,
+    resize_size: int,
+    crop_size: int,
 ):
+    """
+    Given VOC annotation return grid_dim x grid_dim x out_channels label matrix.
+
+    The out_channels are num_bbox_elements + num_classes.
+    """
     img_size = {_: int(val) for _, val in voc_annotation["size"].items()}
     assert img_size["depth"] == 3
     num_classes = len(label_to_idx)
     multiple_boxes_in_grid_cell = False
+    new_img_size = {"width": crop_size, "height": crop_size}
+    ignore_bad_crop = True
 
     # assume only one target box per grid cell;
     label_matrix = torch.zeros(
@@ -213,31 +206,131 @@ def get_targets(
     )
     for item in voc_annotation["object"]:
         label_idx = label_to_idx[item["name"]]
-        coords = (
+        coords = [
             float(item["bndbox"]["xmin"]),
             float(item["bndbox"]["ymin"]),
             float(item["bndbox"]["xmax"]),
             float(item["bndbox"]["ymax"]),
+        ]
+        # calculates new bbox XYXY coords after
+        # T.Compose([T.Resize(resize_size), T.CenterCrop(crop_size)])
+        # are applied to img;
+        coords, bad_crop = resize_and_crop_bbox(
+            bbox=coords,
+            old_w=img_size["width"],
+            old_h=img_size["height"],
+            resize_size=resize_size,
+            crop_size=crop_size,
         )
-        # relative x and y center coords are in (0, grid_dim)
-        # the w and h are in (0, STANDARDISED_IMG_DIM)
+        # don't add this box if cropping left too much out;
+        if bad_crop:
+            continue
+        else:
+            ignore_bad_crop = False
+        # get center coords
+        coords = corners_to_midpoint(coords)
+
         # i and j are index of cell that contains the target box center.
-        coords, (i, j) = midpoint_relative_to_grid(
-            *corners_to_midpoint(*coords),
-            img_size=img_size,
+        i, j = get_grid_cell_idxs(
+            xcenter=coords[0],
+            ycenter=coords[1],
+            img_size=new_img_size,
             grid_dim=grid_dim,
-            standard_img_dim=standard_img_dim,
         )
         # will also keep track if we have multiple target boxes in
         # single grid cell for the current image.
         if label_matrix[i, j, 0] == 1:
             multiple_boxes_in_grid_cell = True
-
+        # add label to relevant grid cell;
         if label_matrix[i, j, -num_classes + label_idx] == 0:
             label_matrix[i, j, -num_classes + label_idx] = 1  # one-hot class;
             label_matrix[i, j, 0] = 1  # object exists;
             label_matrix[i, j, 1:num_bbox_elements] = torch.tensor(coords)
-    return label_matrix, multiple_boxes_in_grid_cell
+    return label_matrix, multiple_boxes_in_grid_cell, ignore_bad_crop
+
+
+def get_resized_wh(old_w: int, old_h: int, size: int):
+    """
+    Sets the smaller dim to size and the other one
+    to int(size * bigger / smaller)
+    """
+    if old_w < old_h:
+        new_w = size
+        new_h = old_h / old_w * size
+    else:
+        new_h = size
+        new_w = old_w / old_h * size
+    return int(new_w), int(new_h)
+
+
+def resize_and_crop_bbox(
+    bbox: MutableSequence,
+    old_w: int,
+    old_h: int,
+    resize_size: int,
+    crop_size: int,
+) -> Tuple[MutableSequence, bool]:
+    """
+    Get the coords of bbox after resize and crop and bool indicator
+    if bbox is usable.
+
+    bbox should be in xmin, ymin, xmax, ymax format.
+
+    Return: (bbox, bad_crop)
+
+    Since we are resizing first and then cropping, it is possible
+    we lose a big part of the bbox if it was close to the edge
+    of the image. If the width or height decrease by more than
+    40%, we return bad_crop=True alongside the bbox, otherwise
+    bad_crop=False.
+    """
+    assert len(bbox) == 4
+    new_w, new_h = get_resized_wh(old_w, old_h, size=resize_size)
+    w_mult = new_w / old_w
+    # shifting is for the crop operation since
+    # we adjust coords by subtracting half ignored region
+    # after the crop;
+    w_shift = (new_w - crop_size) / 2
+    h_mult = new_h / old_h
+    h_shift = (new_h - crop_size) / 2
+    # adjust for resizing;
+    bbox[0], bbox[2] = bbox[0] * w_mult, bbox[2] * w_mult
+    bbox[1], bbox[3] = bbox[1] * h_mult, bbox[3] * h_mult
+    old_bw, old_bh = bbox[2] - bbox[0], bbox[3] - bbox[1]
+    # adjust for cropping;
+    for i, c in enumerate(bbox):
+        if i % 2 == 0:
+            bbox[i] = clip(0, int(c - w_shift), crop_size)
+            assert (
+                bbox[i] <= crop_size
+            ), f"{c=}, {bbox[i]=}, {crop_size=}, {new_w=}, {old_w=}, {old_h=}"
+        else:
+            bbox[i] = clip(0, int(c - h_shift), crop_size)
+            assert (
+                bbox[i] <= crop_size
+            ), f"{c=}, {bbox[i]=}, {crop_size=}, {new_h=}, {old_w=}, {old_h=}"
+    new_bw, new_bh = bbox[2] - bbox[0], bbox[3] - bbox[1]
+    # don't add this box if cropping left too much out;
+    bad_crop = new_bw < old_bw * 0.6 or new_bh < old_bh * 0.6
+    return bbox, bad_crop
+
+
+def corners_to_midpoint(coords):
+    xmin, ymin, xmax, ymax = coords
+    centerx = (xmax + xmin) // 2
+    centery = (ymax + ymin) // 2
+    boxwidth = xmax - xmin
+    boxheight = ymax - ymin
+    return centerx, centery, boxwidth, boxheight
+
+
+def get_grid_cell_idxs(xcenter, ycenter, img_size, grid_dim) -> tuple[int]:
+    """returns grid cell (i, j) coords."""
+    # i and j are in (0, grid_dim)
+    # i,j represents the cell row and cell column
+    i = int(grid_dim * xcenter / img_size["width"])
+    j = int(grid_dim * ycenter / img_size["height"])
+    return i, j
 
 
 def get_all_img_label_matrices(
@@ -245,8 +338,9 @@ def get_all_img_label_matrices(
     grid_dim,
     num_bbox_elements,
     label_to_idx,
+    resize_size,
     ignore_multibox=False,
-    standard_img_dim=224,
+    crop_size=224,
 ):
     """
     Returns list of PIL imgs, list of label_matrix tensors
@@ -258,22 +352,30 @@ def get_all_img_label_matrices(
     label_matrices = []
     multi_box_idxs = []
     imgs = []
+    bad_crop_count = 0
 
     for i, (img, info) in enumerate(dataset):
-        label_matrix, multi_box = get_targets(
+        # since we first resize and then center crop
+        # it might be possible to decrease
+        label_matrix, multi_box, ignore_bad_crop = get_targets(
             info["annotation"],
             grid_dim=grid_dim,
             num_bbox_elements=num_bbox_elements,
             label_to_idx=label_to_idx,
-            standard_img_dim=standard_img_dim,
+            resize_size=resize_size,
+            crop_size=crop_size,
         )
 
         if multi_box:
             multi_box_idxs.append(i)
             if ignore_multibox:
                 continue
+        if ignore_bad_crop:
+            bad_crop_count += 1
+            continue
         label_matrices.append(label_matrix)
         imgs.append(img)
+    print(f"Ignored bad crops: {bad_crop_count}")
     return imgs, label_matrices, multi_box_idxs
 
 